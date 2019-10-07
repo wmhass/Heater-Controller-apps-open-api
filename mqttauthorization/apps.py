394a4811102ee55c69d5d969a4155186c62b8d1b
William Hass <wm.hass@gmail.com>
2019-10-07 08:30:36 -0300

    added new app for mqttauthorization

---
+++ mqttauthorization/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MqttauthorizationConfig(AppConfig):
+    name = 'mqttauthorization'
---
+++ mqttauthorization/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class MqttAcls(models.Model):
+    username = models.CharField(max_length=200)
+    pw = models.CharField(max_length=200)
+    superuser = models.PositiveSmallIntegerField(default=0)
+    # constraints = [
+    #     models.CheckConstraint(
+    #             check=~Q(superuser=0) | ~Q(superuser=1),
+    #             name="system_not_blank")
+    # ]
+
+# class MqttAccount(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
