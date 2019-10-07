013086adfe69121f4a59ce92b05ebc97c255d5a3
William Hass <wm.hass@gmail.com>
2019-10-07 20:54:38 -0300

    removed model

--- mqttauthorization/models.py
+++ mqttauthorization/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-from django.db.models import Q
-
-class MqttAccount(models.Model):
-    username = models.CharField(max_length=40)
-    pw = models.CharField(max_length=100)
-    superuser = models.PositiveSmallIntegerField(default=0)
-
-class MqttAcl(models.Model):
-    username = models.CharField(max_length=40)
-    topic = models.CharField(max_length=100)
-    rw = models.PositiveSmallIntegerField(default=1)
