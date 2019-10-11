01c3031dcb38f4cbced9b65734bda3b20064d6ca
William Hass <wm.hass@gmail.com>
2019-10-11 11:20:45 -0300

    updating app name

--- apps_open_api/urls.py
+++ apps_open_api/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from upload.views import image_upload
+from upload_app.views import image_upload
 
 urlpatterns = [
     path('', image_upload, name='upload'),
